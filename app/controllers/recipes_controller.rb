35d40f969cacb03439660374495164f58ded03d3
Wladimir Oliveira Souza <wladimir5002@gmail.com>
2023-05-31 17:22:09 -0300

    Finalizada tela de mostrar receita

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,5 @@
+class RecipesController < ApplicationController
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+end
--- spec/system/user_view_recipe_details_spec.rb
+++ spec/system/user_view_recipe_details_spec.rb
@@ -2,15 +2,16 @@ require 'rails_helper'
 
 describe 'User view recipe details' do
   it 'sucessfully' do
+    Recipe.create!(name: 'Pão de forma', time_to_prepare:'30 minutos', ingredients: 'trigo, agua, ovo', recipe_type: 'pão')
+    Recipe.create!(name: 'Pudim de pão', time_to_prepare:'45 minutos', ingredients: 'pão, leite, açucar, ovo', recipe_type: 'sobremesa')
 
     visit root_path
     click_on 'Pudim de pão'
 
-    expect(current_path).to eq recipe_path
+    expect(current_path).to eq recipe_path(Recipe.last)
     expect(page).to have_content 'Pudim de pão'
     expect(page).to have_content '45 minutos'
     expect(page).to have_content 'pão, leite, açucar, ovo'
     expect(page).to have_content 'sobremesa'
-
   end
 end
